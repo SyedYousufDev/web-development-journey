ada4cae7d373350b5803ad00d43a4e0dac5a458b
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-03-17 23:38:16 +0500

    Update the code

--- js/part_01/02_task_string_methods.js
+++ js/part_01/02_task_string_methods.js
@@ -15,3 +15,14 @@ console.log("PYTHON to lower:- "+b.toLowerCase());
 //method with arguments
 let c="ilovequran";
 console.log("index of l in ilovequran is : "+ c.indexOf("l"))
+
+//slice method
+console.log("slice method c.slice(-9,-5) in string is : "+ c.slice(-9,-5))
+console.log("slice method c.slice(1) in string is : "+ c.slice(1))
+console.log("slice method c.slice(-5,c.length) in string is : "+ c.slice(-5,c.length))
+
+//replace method
+console.log("replace method c.replace('read','love') in string is : "+  c.replace('love','read'))
+
+//repeat method
+console.log("repeat method c.repeat(5) in string is : "+  c.repeat(5));
