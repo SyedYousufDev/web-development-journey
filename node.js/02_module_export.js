ab30579768f4722f97bfea4b7aee73caba02d255
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-05-06 22:24:13 +0500

    added files where export.module is used with require

---
+++ node.js/02_module_export.js
@@ -0,0 +1,14 @@
+
+const math = require('./math.js');  
+
+
+const num1 = 10;
+const num2 = 5;
+
+console.log("=== Math Operations ===");
+console.log(`Sum of ${num1} and ${num2}: ${math.sum(num1, num2)}`);
+console.log(`Product of ${num1} and ${num2}: ${math.mul(num1, num2)}`);
+console.log(`Value of PI: ${math.PI}`);
+
+
+
---
+++ node.js/math.js
@@ -0,0 +1,7 @@
+const obj = {
+    sum: (a, b) => a + b,
+    mul: (a, b) => a * b,
+    PI: 3.14156
+};
+
+module.exports = obj;
