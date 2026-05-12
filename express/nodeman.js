8ba192f94e7fd0c1c44dbb18efcb2ab2d3c7961d
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-05-12 23:18:25 +0500

    updated the file

--- express/nodeman.js
+++ express/nodeman.js
@@ -1,6 +1,10 @@
 
-// to automatically restart server with code changes
+// Nodemon is a utility tool for Node.js developers that automatically restarts your application whenever it detects file changes in the directory.
+// Instead of manually stopping and restarting your server after every code edit, Nodemon watches your files and handles the restart process for you — saving time and keeping your development flow smooth.
+
+
 
 // npm install -g nodemon
 
 //nodemon -v
+// nodemon is a
