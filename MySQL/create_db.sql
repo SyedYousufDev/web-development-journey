e239ee1ed15c6337e3319f0cb646117db444d8f9
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-05-22 11:23:41 +0500

    MySQL Database Creation

---
+++ MySQL/create_db.sql
@@ -0,0 +1,31 @@
+
+CREATE DATABASE db_name;
+
+DROP DATABASE db_name;
+
+USE db_name;
+
+---------------------------------------------------------------------------------------------
+
+CREATE DATABASE db_name;
+
+CREATE DATABASE IF NOT EXISTS db_name;
+
+
+DROP DATABASE db_name;
+
+DROP DATABASE IF EXISTS db_name;
+
+
+SHOW DATABASES;
+
+SHOW TABLES;
+
+-----------------------------------------------------------------------------------------------
+
+
+-- CREATE DATABASE: Creates a brand-new, empty database container to hold your future data.
+
+-- DROP DATABASE: Permanently deletes a database and instantly wipes out all data inside it forever.
+
+-- USE: Selects the specific database you want to open and work with for your next commands.
