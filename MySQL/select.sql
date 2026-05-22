f5add307131520191cd009a0de20931be3a37013
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-05-22 11:45:44 +0500

    select in sql

---
+++ MySQL/select.sql
@@ -0,0 +1,9 @@
+
+-- SELECT: Used to fetch and display specific columns of data from a table
+SELECT col1, col2 FROM table_name;
+
+-- SELECT *: Uses the asterisk wildcard to fetch and show ALL columns and rows from a table
+SELECT * FROM table_name;
+
+-- use distinct keywords to only give unique values from the all columns
+SELECT * FROM DISTINCT table_name;
