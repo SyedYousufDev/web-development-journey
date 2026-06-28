b4b2872689b3e47141464c1dadece742a58bcc13
SMYousufDev <syedmuhammadyousuf23@gmail.com>
2026-06-28 12:20:59 +0500

    mongodb with express

--- mongodb_with_express/index.js
+++ mongodb_with_express/index.js
@@ -2,13 +2,14 @@ const Chat = require("./models/chat.js");
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 app.use(express.urlencoded({ extended: true }));
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 
 app.use(express.static(path.join(__dirname,"public")));
-
+app.use(methodOverride("_method")); 
 const mongoose = require("mongoose");  
 
 main()
@@ -62,13 +63,47 @@ app.post("/chats",(req,res)=>{
   res.redirect("/chats");
 });
 
+//edit route
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+   let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+})
+
+
+//Update Route
+app.put("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { message: newMsg } = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate( 
+        id,
+        { message: newMsg },
+        { runValidators: true, new: true }
+    );
+
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+
+// Delete Route
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    
+    // Deletes the chat document from MongoDB using its ID
+    let deletedChat = await Chat.findByIdAndDelete(id);
+
+    console.log("Deleted:", deletedChat);
+    res.redirect("/chats");
+});
 
 app.get("/",(req,res)=>{  
-    res.send("Root is Working") 
+    res.render("main.ejs") ;
 })
 
 app.listen(8080,()=>{
     console.log("Server is listening on port 8080");
 });
+
 
  
